1c75cce2ad0b0d54fa432d8dc45f7e20ff7ed29c
angkomuhtar <angkomuhtar@gmail.com>
2023-05-11 12:21:26 +0800

    env

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,17 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize("mam_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+dotenv.config();
+console.log("DB", process.env.DB_HOST);
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 export default db;
